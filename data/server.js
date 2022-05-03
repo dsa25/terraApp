17e24e7d356b3dc86866ac6d02f0618a6e12ad6d
dsa25 <73094994+dsa25@users.noreply.github.com>
2022-05-03 22:21:38 +0500

    data start

--- data/server.js
+++ data/server.js
@@ -2,10 +2,10 @@ const url = "http://127.0.0.1:5000"
 
 export const server = {
   users: url + "/users",
-  allInspects: url + "/allInspects",
-  allNewInspects: url + "/allNewInspects",
   addInspect: url + "/addInspect",
-  getEditedInspects: url + "/getEditedInspects",
   updateInspect: url + "/updateInspect",
-  allVers: url + "/allVers",
+  // allInspects: url + "/allInspects",
+  // allNewInspects: url + "/allNewInspects",
+  // getEditedInspects: url + "/getEditedInspects",
+  // allVers: url + "/allVers",
 }
