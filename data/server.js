1ca3157e39088d195881412414eb48614dcec537
dsa25 <73094994+dsa25@users.noreply.github.com>
2023-05-03 14:01:21 +0500

    add screenshots

--- data/server.js
+++ data/server.js
@@ -1,13 +1,10 @@
 // const url = "http://127.0.0.1:5000"
-// const url = "http://127.0.0.1:80"
-// const url = "http://192.168.0.103:80"
-// const url = "http://146.59.80.129"
 const url = "https://terraserver.dsa25.ru"
 
 export const server = {
   users: url + "/users",
   addInspect: url + "/addInspect",
-  updateInspect: url + "/updateInspect",
+  updateInspect: url + "/updateInspect"
   // allInspects: url + "/allInspects",
   // allNewInspects: url + "/allNewInspects",
   // getEditedInspects: url + "/getEditedInspects",
